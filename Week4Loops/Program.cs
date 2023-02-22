908faab5088e08a05812483dff3b1e4440d16aeb
Kristjan Palm <kpalm@tktk.ee>
2023-02-22 19:01:07 +0200

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do Not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++ ) //i++ = i + 1
+{
+    Console.WriteLine("We dont need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have nice day.");
+
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+
